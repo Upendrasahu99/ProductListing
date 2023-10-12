bbf5702e8f6652037c5f233743b34b870dedbbc0
Upendra Sahu <upendrasahu1199@gmail.com>
2023-10-12 20:46:34 +0530

    [Upendra] Add: Added Search Feature in List Page

--- ProductManagement/Controllers/ProductController.cs
+++ ProductManagement/Controllers/ProductController.cs
@@ -14,10 +14,14 @@ namespace ProductManagement.Controllers
             this.productService = productService;
         }
 
-        public IActionResult ProductList()
+        public IActionResult ProductList(string keyword)
         {
             List<ProductModel> products = productService.GetAll().ToList();
             products = products.OrderByDescending(u => u.CreationDate).ToList();    
+            if(keyword != null)
+            {
+                products = products.Where(u => u.ProductName.Contains(keyword) || u.Description.Contains(keyword) || u.Category.Contains(keyword) || u.Status.Contains(keyword)).ToList();
+            }
             return View(products);
         }
 
